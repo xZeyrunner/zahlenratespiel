db37cdcc60a09005107c3e303f54f7e4574f9adf
Miran126 <miran@hassanwww.de>
2022-08-30 11:25:29 +0200

    Eingegebenen Zahlen aus der html entfernt

--- app.py
+++ app.py
@@ -30,13 +30,13 @@ def index():
             versuche = len(my_tasks)
             aktuelleZahl = my_tasks[-1].content
             if int(my_tasks[-1].content) == randomZahl:
-                return render_template('index.html', tasks=my_tasks, richtig=richtig, aktuelleZahl=aktuelleZahl, versuche=versuche)
+                return render_template('index.html', richtig=richtig, aktuelleZahl=aktuelleZahl, versuche=versuche)
             elif int(my_tasks[-1].content) > randomZahl:
-                return render_template('index.html', tasks=my_tasks, tiefer=tiefer, aktuelleZahl=aktuelleZahl, versuche=versuche)
+                return render_template('index.html', tiefer=tiefer, aktuelleZahl=aktuelleZahl, versuche=versuche)
             elif int(my_tasks[-1].content) < randomZahl:
-                return render_template('index.html', tasks=my_tasks, hoeher=hoeher, aktuelleZahl=aktuelleZahl, versuche=versuche)
+                return render_template('index.html', hoeher=hoeher, aktuelleZahl=aktuelleZahl, versuche=versuche)
         else:
-            return render_template('index.html', tasks=my_tasks)
+            return render_template('index.html')
 
 @app.route('/delete/<int:id>')  #
 def delete(id):                 #
